f70378a1b1a26f2e3238f831b742b0f1a7db29ff
ferdouszitu <ferdousfakir21@gmail.com>
2022-09-01 15:12:09 +0600

    modal search enter btn added

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 }
-const displayPhones = phones => {
+const displayPhones = (phones, dataLimit) => {
     const phonesContainer = document.getElementById('phone-container');
     phonesContainer.textContent = '';
-    // display 20 only
-    phones = phones.slice(0, 20);
+    // display 10 only
+    const showAll = document.getElementById('show-all');
+    if (dataLimit && phones.length > 10) {
+        phones = phones.slice(0, 10);
+        showAll.classList.remove('d-none');
+    }
+    else {
+        showAll.classList.add('d-none');
+
+    }
+
     // display no phones
     const noPhone = document.getElementById('no-found-msg');
     if (phones.length === 0) {
@@ -24,10 +33,11 @@ const displayPhones = phones => {
         phoneDiv.innerHTML = `
                             <div class="card p-4">
                             <img src="${phone.image}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                            <h5 class="card-title">${phone.phone_name}</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural
+                        <div class="card-body">
+                        <h5 class="card-title">${phone.phone_name}<h5>
+                         with supporting text below as a natural
                              lead-in to additional content. This content is a little bit longer.</p>
+                             <button onclick="loadPhoneDetails('${phone.slug}')"  href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
                             </div>
                         </div>
         `
@@ -36,16 +46,28 @@ const displayPhones = phones => {
     // stop spinner or loader
     toggleSpinner(false);
 }
-
+const processSearch = (dataLimit) => {
+    toggleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhone(searchText, dataLimit);
+}
 
 // handle search button click
 document.getElementById('btn-search').addEventListener('click', function () {
     // start spinner or loader
-    toggleSpinner(true);
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    loadPhone(searchText);
+    processSearch(10)
 })
+
+//search input field button handler
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+    console.log(e.key)
+    if (e.key === 'Enter') {
+        processSearch(10)
+        // code for enter
+    }
+});
+
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('spinner');
     if (isLoading) {
@@ -56,5 +78,27 @@ const toggleSpinner = isLoading => {
     }
 }
 
+document.getElementById('btn-show-all').addEventListener('click', function () {
+    processSearch()
+})
+const loadPhoneDetails = async id => {
+    const url = ` https://openapi.programming-hero.com/api/phone/${id}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetail(data.data);
+}
+const displayPhoneDetail = phone => {
+    console.log(phone);
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+    const phoneDetail = document.getElementById('phone-detail');
+    phoneDetail.innerHTML = `
+    <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'No Release Date '} </p>
+    <p>MainFeatures : ${phone.mainFeatures ? phone.mainFeatures.storage : 'No information found'}, ${phone.mainFeatures ? phone.mainFeatures.displaySize : 'No information found '} </p>
+    <p>Others: ${phone.others ? phone.others.WLAN : 'No information found  '} </p>
+    
+`
 
-// loadPhone();
+}
+
+loadPhone('apple');
